366d2717ded65f13c18bbcf5452afd7bd6db7b80
dns-lector <dns.lector@ukr.net>
2024-02-12 19:40:55 +0200

    JSP

--- src/main/java/step/learning/servlets/HomeServlet.java
+++ src/main/java/step/learning/servlets/HomeServlet.java
@@ -1,18 +1,27 @@
 package step.learning.servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("")
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setAttribute( "page-body", "home.jsp" ) ;
+        req.getRequestDispatcher( "WEB-INF/_layout.jsp" )  // перенаправлення (внутрішнє)
+                .forward(req, resp);
     }
 }
 /*
 Сервлет - клас для мережних задач,
 HttpServlet - аналог контролерів у веб-проєктах
+Д.З. Створити веб-проєкт, добитись його запуску
+Реалізувати сервлет для нової сторінки "privacy",
+забезпечити її відображення через шаблон (layout)
+Додати посилання на неї до головної сторінки.
+* наповнити сторінку "privacy" контентом
  */
--- src/main/webapp/WEB-INF/_layout.jsp
+++ src/main/webapp/WEB-INF/_layout.jsp
@@ -1,10 +1,16 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%
+    String pageBody = (String) request.getAttribute( "page-body" ) ;
+    if( pageBody == null ) {
+        pageBody = "home.jsp" ;   // default page
+    }
+%>
 <!doctype html>
 <html>
 <head>
     <title>Java web</title>
 </head>
 <body>
-
+    <jsp:include page="<%= pageBody %>" />
 </body>
 </html>
