62c502aafcd107348d59811c3f7a96aaa8930b37
dns-lector <157026463+dns-lector@users.noreply.github.com>
2024-03-01 17:48:25 +0200

    DB -- 2

--- src/main/java/step/learning/services/db/MariaDbService.java
+++ src/main/java/step/learning/services/db/MariaDbService.java
@@ -1,12 +1,19 @@
 package step.learning.services.db;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,12 +30,38 @@ public class MariaDbService implements DbService {
     @Override
     public Connection getConnection() {
         if( connection == null ) {
+            // демонстрація роботи з ресурсами (папка resources)
+            // вміст папки входить у deploy у складі папки target/classes
+            // звернутись до цієї папки можна через завантажувач класів
+            // сам завантажувач можна взяти з будь-якого об'єкту
+            JsonObject dbConfig;
+            try( InputStream reader = this.getClass().getClassLoader().getResourceAsStream("appsettings.json")){
+                dbConfig = JsonParser.parseString( readStreamToEnd(
+                            Objects.requireNonNull(reader) ) )
+                        .getAsJsonObject()
+                        .getAsJsonObject("db")
+                        .getAsJsonObject("MariaDB");
+            }
+            catch (IOException ex) {
+                logger.log( Level.SEVERE, ex.getMessage() );
+                return null ;
+            }
             try {
                 mySqlDriver = new com.mysql.cj.jdbc.Driver() ;
                 DriverManager.registerDriver( mySqlDriver ) ;
-                String connectionString = "jdbc:mysql://localhost:3306/java_111" +
-                        "?useUnicode=true&characterEncoding=UTF-8" ;
-                connection = DriverManager.getConnection( connectionString, "root", "" ) ;
+                String connectionString = String.format( Locale.UK,
+                        "jdbc:%s://%s:%d/%s?%s",
+                        dbConfig.get("protocol").getAsString(),
+                        dbConfig.get("host").getAsString(),
+                        dbConfig.get("port").getAsInt(),
+                        dbConfig.get("scheme").getAsString(),
+                        dbConfig.get("params").getAsString()
+                ) ;
+                connection = DriverManager.getConnection(
+                        connectionString,
+                        dbConfig.get("user").getAsString(),
+                        dbConfig.get("password").getAsString()
+                ) ;
             }
             catch (SQLException e) {
                 throw new RuntimeException(e);
@@ -36,7 +69,6 @@ public class MariaDbService implements DbService {
         }
         return connection ;
     }
-
     @Override
     public void closeConnection() {
         try {
@@ -50,5 +82,14 @@ public class MariaDbService implements DbService {
         catch (SQLException ex) {
             logger.log( Level.WARNING, ex.getMessage() );
         }
+    }
+    private String readStreamToEnd( InputStream inputStream ) throws IOException {
+        final byte[] buffer = new byte[32 * 1024];  // 32k
+        ByteArrayOutputStream byteBuilder = new ByteArrayOutputStream() ;
+        int len ;
+        while( ( len = inputStream.read( buffer ) ) > -1 ) {
+            byteBuilder.write( buffer, 0, len ) ;
+        }
+        return byteBuilder.toString() ;
     }
 }
--- src/main/java/step/learning/servlets/HomeServlet.java
+++ src/main/java/step/learning/servlets/HomeServlet.java
@@ -15,31 +15,9 @@ import java.util.Objects;
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // демонстрація роботи з ресурсами (папка resources)
-        // вміст папки входить у deploy у складі папки target/classes
-        // звернутись до цієї папки можна через завантажувач класів
-        // сам завантажувач можна взяти з будь-якого об'єкту
-        try(InputStream reader = this.getClass().getClassLoader().getResourceAsStream("appsettings.json")){
-            System.out.println(
-                    readStreamToEnd(
-                            Objects.requireNonNull(reader) ) ) ;
-        }
-        catch (IOException ex) {
-            System.err.println( ex.getMessage() );
-        }
-
         req.setAttribute( "page-body", "home.jsp" ) ;
         req.getRequestDispatcher( "WEB-INF/_layout.jsp" )  // перенаправлення (внутрішнє)
                 .forward(req, resp);
-    }
-    private String readStreamToEnd( InputStream inputStream ) throws IOException {
-        final byte[] buffer = new byte[32 * 1024];  // 32k
-        ByteArrayOutputStream byteBuilder = new ByteArrayOutputStream() ;
-        int len ;
-        while( ( len = inputStream.read( buffer ) ) > -1 ) {
-            byteBuilder.write( buffer, 0, len ) ;
-        }
-        return byteBuilder.toString() ;
     }
 }
 /* req.getServletContext().getRealPath("/")
